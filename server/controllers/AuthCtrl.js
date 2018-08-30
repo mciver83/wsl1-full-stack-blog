b031aa301a60d556f1b52aeef9f7e72f1541ba0d
Mark McIver <mark@Marks-MacBook-Pro.local>
2018-08-30 14:50:30 -0600

    fix auth0 redirect uri protocol

--- server/controllers/AuthCtrl.js
+++ server/controllers/AuthCtrl.js
@@ -11,7 +11,7 @@ module.exports = {
         client_secret: process.env.AUTH0_CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: `https://${req.headers.host}/auth/callback`
+        redirect_uri: `${req.protocol}://${req.headers.host}/auth/callback`
       }
 
       //here we are given the access token from auth0
